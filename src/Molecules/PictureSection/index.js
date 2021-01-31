bf1b35be5e76d531b8f4c8937b3812a803e76cf9
Abhishek P <abhishek.p@piktorlabs.com>
2021-01-31 20:46:56 +0530

    Instagram backup added

--- src/Molecules/PictureSection/index.js
+++ src/Molecules/PictureSection/index.js
@@ -15,23 +15,30 @@ const Container = styled.section`
 const PicturesSection = () => {
 	const [images, setListOfImages] = useState([])
 
-	useEffect(() => {
-		fetch(instagramUrl)
-			.then((res) => res.json())
-			.then((result) => {
-				let listOfImages =
-					result?.graphql?.user?.edge_owner_to_timeline_media?.edges
-				listOfImages = listOfImages
-					.filter((item, i) => i < 4)
-					.map((i) => {
-						const { display_url, edge_media_to_caption } = i.node
-						return {
-							image: display_url,
-							caption: edge_media_to_caption.edges[0].node.text,
-						}
-					})
-				setListOfImages(listOfImages)
+	const convertDataToState = (data) => {
+		let listOfImages = data?.graphql?.user?.edge_owner_to_timeline_media?.edges
+		listOfImages = listOfImages
+			.filter((item, i) => i < 4)
+			.map((i) => {
+				const { display_url, edge_media_to_caption } = i.node
+				return {
+					image: display_url,
+					caption: edge_media_to_caption.edges[0].node.text,
+				}
 			})
+		setListOfImages(listOfImages)
+	}
+
+	useEffect(() => {
+		;(async () => {
+			try {
+				const result = await fetch(instagramUrl).then((res) => res.json())
+				convertDataToState(result)
+			} catch (error) {
+				const localListOfImages = await import('../../content/data.json')
+				convertDataToState(localListOfImages.default)
+			}
+		})()
 	}, [])
 
 	return (
