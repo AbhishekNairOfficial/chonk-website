580c909d3447422a8fdb3ce3f01e31712a9ef6c4
Abhishek P <abhishek.p@piktorlabs.com>
2021-02-01 16:32:09 +0530

    Added caption

--- src/atoms/Image/index.js
+++ src/atoms/Image/index.js
@@ -4,11 +4,31 @@ const Image = styled.img`
 	max-width: 100%;
 `
 
+const Container = styled.div`
+	position: relative;
+	&:hover > div {
+		display: block;
+	}
+`
+
+const Caption = styled.div`
+	position: absolute;
+	padding: 20px;
+	color: ${(p) => p.theme.palette.white};
+	background-color: ${(p) => p.theme.palette.black + 'CC'};
+	height: 100%;
+	overflow-y: auto;
+	font-size: 1em;
+	line-height: 1.45em;
+	display: none;
+`
+
 const InstagramPost = ({ caption, image }) => {
 	return (
-		<div>
+		<Container>
+			<Caption>{caption}</Caption>
 			<Image src={image} alt='instagram post' />
-		</div>
+		</Container>
 	)
 }
 
